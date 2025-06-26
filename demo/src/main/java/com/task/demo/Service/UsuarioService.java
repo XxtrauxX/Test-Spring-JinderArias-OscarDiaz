c4f5569efd2e64a19b34e06ccfce721b1cc5e95e
Jinderarias1303 <jindergabrielariasanchez@gmail.com>
2025-06-25 21:23:40 -0500

    ...

--- demo/src/main/java/com/task/demo/Service/UsuarioService.java
+++ demo/src/main/java/com/task/demo/Service/UsuarioService.java
@@ -1,5 +1,13 @@
 package com.task.demo.Service;
 
-public class UsuarioService {
+import com.task.demo.DTO.UsuarioResponseDTO;
+import com.task.demo.DTO.UsuarioRequestDTO;
 
+import java.util.List;
+
+public interface UsuarioService {
+    UsuarioResponseDTO registrarUsuario(UsuarioRequestDTO usuarioRequest);
+    UsuarioResponseDTO obtenerUsuarioPorId(Long id);
+    List<UsuarioResponseDTO> obtenerTodos();
+    void cambiarEstado(Long id, boolean estado);
 }
