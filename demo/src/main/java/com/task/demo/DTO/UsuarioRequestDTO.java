3ebb410298ddceaa52322932cd86cf3e652a28e0
XxtrauxX <aaronjones2429@gmail.com>
2025-06-25 21:21:24 -0500

    subiendo mas cambbios

---
+++ demo/src/main/java/com/task/demo/DTO/UsuarioRequestDTO.java
@@ -0,0 +1,13 @@
+package com.task.demo.DTO;
+
+import com.task.demo.Entity.Rol;
+
+import lombok.Data;
+
+@Data
+public class UsuarioRequestDTO {
+    private String username;
+    private String password;
+
+
+}
